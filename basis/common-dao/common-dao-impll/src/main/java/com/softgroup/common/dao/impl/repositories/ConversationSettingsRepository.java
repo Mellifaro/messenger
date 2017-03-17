46f43b9fcc24b8751163da3724c19f0ae7e816b9
Mellifaro <housse1109@gmail.com>
2017-03-17 23:11:20 +0200

    Created entities and dao interfaces

---
+++ basis/common-dao/common-dao-impll/src/main/java/com/softgroup/common/dao/impl/repositories/ConversationSettingsRepository.java
@@ -0,0 +1,10 @@
+package com.softgroup.common.dao.impl.repositories;
+
+import com.softgroup.common.dao.api.entities.ConversationSettingsEntity;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+/**
+ * Created by Виктор on 17.03.2017.
+ */
+public interface ConversationSettingsRepository extends PagingAndSortingRepository<ConversationSettingsEntity, String>{
+}
