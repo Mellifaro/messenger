50864f400eacb43bfde480a1c76d34d2066dbac5
Mellifaro <housse1109@gmail.com>
2017-03-27 23:14:55 +0300

    Created mvc and security config. Created controllers

---
+++ frontend/rest/rest-impl/src/main/java/com/softgroup/rest/impl/securityConfig/AppSecurityConfig.java
@@ -0,0 +1,73 @@
+package com.softgroup.rest.impl.securityConfig;
+
+/**
+ * Created by Виктор on 27.03.2017.
+ */
+
+import com.softgroup.rest.impl.tokenFilter.JwtAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+/**
+ * Created by Виктор on 20.03.2017.
+ */
+
+@Configuration
+@EnableWebSecurity
+public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private AuthenticationEntryPoint restAuthenticationEntryPoint;
+
+    @Autowired
+    private AuthenticationSuccessHandler jwtAuthenticationSuccessHandler;
+
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Autowired
+    private AuthenticationManager jwtAuthenticationManager;
+
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .antMatchers("/register", "/sms_confirm", "/login");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .exceptionHandling()
+                .authenticationEntryPoint(restAuthenticationEntryPoint)
+                .and()
+                .authorizeRequests().anyRequest().authenticated()
+                .and()
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+}
+
+//    @Bean
+//    public RestAuthenticationSuccessHandler mySuccessHandler(){
+//        return new RestAuthenticationSuccessHandler();
+//    }
+//    @Bean
+//    public SimpleUrlAuthenticationFailureHandler myFailureHandler(){
+//        return new SimpleUrlAuthenticationFailureHandler();
+//    }
+//
+//    @Override
+//    protected AuthenticationManager authenticationManager() throws Exception {
+//        return super.authenticationManager();
+//    }
---
+++ frontend/rest/rest-impl/src/main/java/com/softgroup/rest/impl/tokenFilter/JwtAuthenticationManager.java
@@ -0,0 +1,29 @@
+package com.softgroup.rest.impl.tokenFilter;
+
+import com.softgroup.common.token.api.TokenService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by Виктор on 23.03.2017.
+ */
+@Component
+public class JwtAuthenticationManager implements AuthenticationManager {
+
+    @Autowired
+    private TokenService tokenService;
+
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        JwtAuthenticationToken jwtToken = (JwtAuthenticationToken)authentication;
+        boolean isAuthenticated = tokenService.validateToken(jwtToken.getJwtToken(), "device_token");
+        authentication.setAuthenticated(isAuthenticated);
+        return authentication;
+    }
+
+    public boolean supports(Class<?> auth) {
+        return auth.equals(JwtAuthenticationToken.class);
+    }
+}
