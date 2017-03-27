5faeb1927fe69e030d0d2754015f0dc9aa86c703
Mellifaro <housse1109@gmail.com>
2017-03-27 19:43:03 +0300

    Token service

---
+++ basis/common-token/common-token-api/src/main/java/com/softgroup/common/token/api/TokenService.java
@@ -0,0 +1,13 @@
+package com.softgroup.common.token.api;
+
+/**
+ * Created by Виктор on 20.03.2017.
+ */
+public interface TokenService {
+
+    String generateToken(String userId, Long timeleft, String typeToken);
+
+    String getUserId(String jwtToken);
+
+    boolean validateToken(String jwtToken, String type);
+}
---
+++ basis/common-token/common-token-impl/src/main/java/com/softgroup/token/impl/TokenServiceImpl.java
@@ -0,0 +1,65 @@
+package com.softgroup.token.impl;
+
+
+import com.softgroup.common.token.api.TokenService;
+import io.jsonwebtoken.*;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+/**
+ * Created by Виктор on 22.03.2017.
+ */
+@Component
+public class TokenServiceImpl implements TokenService{
+    private static final String KEY = "9F5E2F36DAE495B3897AF86BCBCD386D8912DE9E6B4F0FF472EB371C3F095A1D";
+    private static final SignatureAlgorithm SIGNATURE_ALGORITHM = SignatureAlgorithm.HS256;
+
+    @Override
+    public String generateToken(String userId, Long timeleft, String typeToken) {
+        Claims claims = Jwts.claims().setSubject(userId);
+        claims.put("type", typeToken);
+
+
+        JwtBuilder builder = Jwts.builder()
+                .setClaims(claims)
+                .signWith(SIGNATURE_ALGORITHM, KEY);
+
+        if(timeleft >= 0){
+            long expMillis = System.currentTimeMillis() + timeleft;
+            Date exp = new Date(expMillis);
+            builder.setExpiration(exp);
+        }
+        return builder.compact();
+    }
+
+    @Override
+    public String getUserId(String jwtToken) {
+        try{
+            Claims body = Jwts.parser()
+                    .setSigningKey(KEY)
+                    .parseClaimsJws(jwtToken)
+                    .getBody();
+            return (String) body.get("sub");
+        }catch (ClassCastException | JwtException e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @Override
+    public boolean validateToken(String jwtToken, String type) {
+        try{
+            Claims body = Jwts.parser()
+                    .setSigningKey(KEY)
+                    .parseClaimsJws(jwtToken)
+                    .getBody();
+            return body != null
+                    && body.get("type").equals("device_token")
+                    && (Long) body.get("exp") > System.currentTimeMillis();
+        }catch (ClassCastException | JwtException e){
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
