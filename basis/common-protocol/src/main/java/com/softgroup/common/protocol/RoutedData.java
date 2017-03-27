1f4973c7a3e936e2f0e56ddf4ea4e0f064e5800b
Mellifaro <housse1109@gmail.com>
2017-03-27 20:24:18 +0300

    Created RoutedData and changed RoutedAction

---
+++ basis/common-protocol/src/main/java/com/softgroup/common/protocol/RoutedData.java
@@ -0,0 +1,29 @@
+package com.softgroup.common.protocol;
+
+import java.io.Serializable;
+
+/**
+ * Created by Виктор on 27.03.2017.
+ */
+public class RoutedData implements Serializable{
+    private static final long serialVersionUID = 3930278156237773465L;
+
+    private String userId;
+    private String sessionUUID;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getSessionUUID() {
+        return sessionUUID;
+    }
+
+    public void setSessionUUID(String sessionUUID) {
+        this.sessionUUID = sessionUUID;
+    }
+}
